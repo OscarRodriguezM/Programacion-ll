1f04e3cbbb454ebaf6ea91f99b31d7286b0cc060
OscarRodriguezM <112915527+OscarRodriguezM@users.noreply.github.com>
2022-09-20 21:54:14 -0500

    Add files via upload

---
+++ PracticaClase20deSeptiembre.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+
+using namespace std;
+
+int add(int x, int y)
+{
+    return x, y;
+}
+void printResult(int z)
+{
+    cout<<"La Respuesta es: "<<z<<'\n';
+}
+int getUserInput()
+{
+    cout<<"Ingrese un numero: ";
+    int x{};
+    cin>>x;
+    return --x;
+}
+int main()
+{
+    int x{ getUserInput()};
+    int y{ getUserInput()};
+    
+    int z{add(x, y) };
+    printResult(z);
+    
+    return 0;
+}
