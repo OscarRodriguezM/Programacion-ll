eef6fcec81e6491cb655f3e8407ebac122492605
OscarRodriguezM <112915527+OscarRodriguezM@users.noreply.github.com>
2022-09-21 20:34:56 -0500

    Delete Actividad 13.cpp

--- Actividad 13.cpp
+++
@@ -1,7 +0,0 @@
-std :: cout << " Hello world ! " ;
-std :: cout
-std :: cout
-          std :: cout <<
-     << " Hello world ! " ;
-                        << " Hello world ! " ;
-                               " Hello world ! " ;
